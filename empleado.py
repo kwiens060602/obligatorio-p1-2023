3f41b8a4fe23223cfe42d5c0bf4519b4918072c2
kwiens060602 <149837392+kwiens060602@users.noreply.github.com>
2023-11-16 14:45:18 -0300

    Add files via upload

--- empleado.py
+++ empleado.py
@@ -1,11 +1,11 @@
 class Empleado:
-    def __init__(self, nombre_empleado, cedula_empleado, fecha_de_nacimiento_empleado, pais_de_nacimiento_empleado, salario_empleado, tipo_empleado):
+    def __init__(self, nombre_empleado, cedula_empleado, fecha_de_nacimiento_empleado, pais_de_nacimiento_empleado, salario_empleado):
        self._nombre_empleado = nombre_empleado
        self._cedula_empleado = cedula_empleado 
        self._fecha_de_nacimiento = fecha_de_nacimiento_empleado
        self._pais_de_nacimiento_empleado = pais_de_nacimiento_empleado
        self._salario_empleado = salario_empleado 
-       self._tipo_empleado = tipo_empleado
+      
        
     @property
     def nombre_empleado(self):
@@ -19,7 +19,5 @@ class Empleado:
     @property
     def salario(self):
         return self._salario_empleado
-    @property
-    def tipo_empleado(self):
-        return self._tipo_empleado 
+    
        
